1f981cf1f5867a6c6adfd7cc69c4c28c9d9d72d8
root <root@gavin>
2024-02-20 17:35:21 -0600

    try1

--- pipeline/apis/0-passengers.py
+++ pipeline/apis/0-passengers.py
@@ -5,18 +5,37 @@ import requests
 
 def availableShips(passenger_count):
     '''fvail ship number'''
-    base_url = "https://swapi.dev/api/starships/"
-    ships = []
+    API_ROOT = "https://swapi-api.alx-tools.com/api/"
+    page = 1
+    results, ships_list, passengers_list = [], [], []
 
-    while base_url:
-        response = requests.get(base_url)
-        data = response.json()
+    response = requests.get(API_ROOT + 'starships/?page={}'.format(page))
+    ships_data = response.json()
 
-        for data in data.get('results', []):
-            passengers = data.get('passengers', '0')
-            if passengers.isdigit() and int(passengers) >= passenger_count:
-                ships.append(data)
+    # Collecting all of the information from the api
+    while ships_data['next']:
+        response = requests.get(API_ROOT + 'starships/?page={}'.format(page))
+        ships_data = response.json()
+        ships_list.extend([ship['name'] for ship in ships_data['results']])
+        passengers_list.extend([ship['passengers']
+                                for ship in ships_data['results']])
+        page += 1
 
-        base_url = data.get('next')
+    # Clean up the passengers list so all are INTS
+    for i in range(0, len(passengers_list)):
+        passengers_list[i] = passengers_list[i].replace(',', '')
+        if passengers_list[i] == 'n/a':
+            passengers_list[i] = -1
+        if passengers_list[i] == 'unknown':
+            passengers_list[i] = -1
+        passengers_list[i] = int(passengers_list[i])
 
-    return ships
+    # Combine ships list and passengers list to a dictionary
+    ship_passenger_dict = dict(zip(ships_list, passengers_list))
+
+    # Filter dictionary to keep valid ships
+    for ship, passengers in ship_passenger_dict.items():
+        if int(passengers) >= passengerCount:
+            results.append(ship)
+
+    return results
