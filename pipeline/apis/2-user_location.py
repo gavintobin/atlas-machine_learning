18df02e5f892e344ba612c615af4eb487a649aff
root <root@gavin>
2024-02-20 17:24:21 -0600

    try1

--- pipeline/apis/2-user_location.py
+++ pipeline/apis/2-user_location.py
@@ -5,14 +5,27 @@ import requests
 import time
 
 
-if __name__ == '__main__':
-    ''''user loc'''
-    res = requests.get(sys.argv[1])
+if __name__ == "__main__":
+    import requests
+    import sys
+    import time
 
-    if res.status_code == 200:
-        print(res.json()['location'])
-    elif res.status_code == 404:
+    if len(sys.argv) != 2:
+        print("Usage: ./2-user_location https://api.github.com/users/<user>")
+
+    API = sys.argv[1]
+
+    response = requests.get(API)
+
+    user_data = response.json()
+
+    if response.status_code == 200:
+        print(user_data['location'])
+
+    elif response.status_code == 404:
         print("Not found")
-    if res.status_code == 403:
-        time = (int(res.headers['X-RateLimit-Reset']) - int(time.time()))
-        print("Reset in {} min".format(time//60))
+
+    elif response.status_code == 403:
+        reset_time = response.headers['X-RateLimit-Reset']
+        now = int(time.time())
+        print("Reset in {} min".format((int(reset_time) - now) // 60))
