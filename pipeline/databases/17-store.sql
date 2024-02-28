28bd0e3b7e61cc6bfd762db9998b0ada06f064c6
root <root@gavin>
2024-02-28 13:10:50 -0600

    try1

---
+++ pipeline/databases/17-store.sql
@@ -0,0 +1,9 @@
+-- store
+CREATE TRIGGER decrease_quantity_after_order
+AFTER INSERT ON orders
+FOR EACH ROW
+BEGIN
+    UPDATE items
+    SET quantity = quantity - NEW.quantity_ordered
+    WHERE id = NEW.item_id;
+END;
