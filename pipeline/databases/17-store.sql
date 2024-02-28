fff2c3e574724054dffe05f84db64c8cb0129b35
root <root@gavin>
2024-02-28 13:59:52 -0600

    try1

--- pipeline/databases/17-store.sql
+++ pipeline/databases/17-store.sql
@@ -1,9 +1,13 @@
 -- store
-CREATE TRIGGER decrease_quantity_after_order
+DELIMITER //
+CREATE TRIGGER decrease_item_quantity
 AFTER INSERT ON orders
 FOR EACH ROW
 BEGIN
+    -- Decrease the quantity of the item in the items table
     UPDATE items
-    SET quantity = quantity - NEW.quantity_ordered
-    WHERE id = NEW.item_id;
+    SET quantity = quantity - NEW.number
+    WHERE name = NEW.item_name;
 END;
+//
+DELIMITER
