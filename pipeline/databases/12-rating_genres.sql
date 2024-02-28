4e4f01bfbdcb75511a8b4e859b21a5359495c19f
root <root@gavin>
2024-02-28 12:36:40 -0600

    try1

--- pipeline/databases/12-rating_genres.sql
+++ pipeline/databases/12-rating_genres.sql
@@ -1,6 +1,6 @@
 -- list all genres
 SELECT tv_genres.name AS name, SUM(tv_show_ratings.rate) AS rating
-FROM tv_shows
+FROM tv_genres
 JOIN tv_show_genres on tv_genres.id = tv_show_genres.genre_id
 JOIN tv_show_ratings ON tv_show_genres.show_id = tv_show_ratings.show_id
 GROUP BY tv_genres.name
