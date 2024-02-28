282f0d55d2e78f1599aa1fee5d92de3961233800
root <root@gavin>
2024-02-28 12:15:19 -0600

    try1

---
+++ pipeline/databases/10-count_shows_by_genre.sql
@@ -0,0 +1,7 @@
+-- count by genre
+SELECT tv_genres.name 'genre', COUNT(tv_show_genres.genre_id) 'number_of_shows'
+    FROM tv_genres
+    INNER JOIN tv_show_genres
+        ON tv_genres.id = tv_show_genres.genre_id
+    GROUP BY tv_genres.name
+    ORDER BY COUNT(tv_show_genres.genre_id) DESC
