84505fb549d2bc4d2dff65cf2c9bdacfa027bf5c
root <root@gavin>
2024-02-28 13:30:39 -0600

    try1

--- pipeline/databases/9-no_genre.sql
+++ pipeline/databases/9-no_genre.sql
@@ -1,6 +1,6 @@
 -- no genre shwos
 SELECT tv_shows.title, tv_show_genres.genre_id
-FROM tv_shows
-RIGHT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
-WHERE tv_show_genres.genre_id IS  NULL
-ORDER BY tv_shows.title, tv_show_genres.genre_id
+FROM tv_show_genres
+RIGHT JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
+WHERE tv_show_genres.show_id IS NULL
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC
