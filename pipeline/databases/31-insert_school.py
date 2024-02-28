e06967966a6dbfde0f60beb1ea8dbc9bc6429424
root <root@gavin>
2024-02-28 16:29:42 -0600

    try2

--- pipeline/databases/31-insert_school.py
+++ pipeline/databases/31-insert_school.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
-'''task 32'''
+'''task 32 maybe this is long enough'''
 
 
 def insert_school(mongo_collection, **kwargs):
-    '''new document'''
+    '''new document maybe this is long enough'''
      return mongo_collection.insert_one(kwargs).inserted_id
