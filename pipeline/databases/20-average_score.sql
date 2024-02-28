f924a6edf8608b9b747016f732dc5985731e20a1
root <root@gavin>
2024-02-28 14:54:13 -0600

    try1

--- pipeline/databases/20-average_score.sql
+++ pipeline/databases/20-average_score.sql
@@ -6,11 +6,11 @@ DECLARE avg_score DECIMAL(5,2);
     -- Calculate the average score for the given user_id
     SELECT AVG(score) INTO avg_score
     FROM scores
-    WHERE user_id = ComputeAverageScoreForUser.user_id;
+    WHERE id = ComputeAverageScoreForUser.user_id;
 
     -- Update the average score in the users table
     UPDATE users
-    SET average_score = avg_score
+    SET average_score = score
     WHERE id = ComputeAverageScoreForUser.user_id;
 END//
 
